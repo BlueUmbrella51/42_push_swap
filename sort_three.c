8d92bfd6a5a745917eb9155523ca4a172eb13a9e
Laure Ravier <photoravier@gmail.com>
2020-02-25 17:03:56 +0100

    fixed returning zero if half was already sorted in sort short

--- sort_three.c
+++ sort_three.c
@@ -6,7 +6,7 @@
 /*   By: lravier <lravier@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/20 13:45:01 by lravier        #+#    #+#                */
-/*   Updated: 2020/02/20 14:29:40 by lravier       ########   odam.nl         */
+/*   Updated: 2020/02/25 16:44:25 by lravier       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,18 +31,20 @@ int				sort_three(t_sort **stacks, t_list **solution)
 	int	c;
 
 	set_data(&a, &b, &c, &(*stacks)->a);
+	if (a <= b && b <= c)
+		return (1);
 	if (a <= b && b >= c && c >= a)
 	{
 		return (add_instruction(SA, stacks, solution)
 		&& add_instruction(RA, stacks, solution));
 	}
-	else if (a >= b && b <= c && c >= a)
+	if (a >= b && b <= c && c >= a)
 		return (add_instruction(SA, stacks, solution));
-	else if (a <= b && b >= c && a >= c)
+	if (a <= b && b >= c && a >= c)
 		return (add_instruction(RRA, stacks, solution));
-	else if (a >= b && b <= c && a >= c)
+	if (a >= b && b <= c && a >= c)
 		return (add_instruction(RA, stacks, solution));
-	else if (a >= b && b >= c)
+	if (a >= b && b >= c)
 	{
 		return (add_instruction(SA, stacks, solution) &&
 		add_instruction(RRA, stacks, solution));
@@ -57,18 +59,20 @@ int				rev_sort_three(t_sort **stacks, t_list **solution)
 	int	c;
 
 	set_data(&a, &b, &c, &(*stacks)->b);
+	if (a >= b && b >= c)
+		return (1);
 	if (a <= b && b <= c)
 	{
 		return (add_instruction(SB, stacks, solution)
 		&& add_instruction(RRB, stacks, solution));
 	}
-	else if (a <= b && b >= c && a <= c)
+	if (a <= b && b >= c && a <= c)
 		return (add_instruction(RB, stacks, solution));
-	else if (a >= b && b <= c && a <= c)
+	if (a >= b && b <= c && a <= c)
 		return (add_instruction(RRB, stacks, solution));
-	else if (a <= b && b >= c && a >= c)
+	if (a <= b && b >= c && a >= c)
 		return (add_instruction(SB, stacks, solution));
-	else if (a >= b && b <= c && a >= c)
+	if (a >= b && b <= c && a >= c)
 	{
 		return (add_instruction(SB, stacks, solution)
 		&& add_instruction(RB, stacks, solution));
